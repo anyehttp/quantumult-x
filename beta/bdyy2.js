f994ae44ec7a1cf59462886eafbcca6c13c74608
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-19 22:18:32 +0800

    Create bdyy2.js

---
+++ beta/bdyy2.js
@@ -0,0 +1,23 @@
+[rewrite_local]
+^https:\/\/bd-api\.kuwo\.cn\/api\/service\/home\/index url script-response-body
+https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/bdyy2.js
+
+[mitm]
+hostname = bd-api.kuwo.cn
+
+
+var body = $response.body;
+var anye = JSON.parse(body);
+
+const ay = /api\/service\/home\/index/;
+
+if(ay.test($request.url))
+{
+    anye.data.list = [];
+    if(anye.data.list && anye.data.list.length >0)
+    {
+        anye.data.list = anye.data.list.filter(home => home.name !== "个性化歌单" || home.name !== "偶遇心动歌单" || home.name !== "波点排行榜");
+    }
+}
+
+$done({body: JSON.stringify(anye)});
