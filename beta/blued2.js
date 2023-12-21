0d705cb11eed4af8cc30d8e2fd6298449bdc7fba
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-21 17:55:00 +0800

    更新 blued2.js

--- beta/blued2.js
+++ beta/blued2.js
@@ -1,32 +1,28 @@
 /*************************************
 [rewrite_local]
-^https:\/\/social\.blued\.cn\/users\?birth_time url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/blued2.js
+https://social.blued.cn/users? url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/blued2.js
 
 [mitm]
 hostname = social.blued.cn
 *************************************/
 var anye = JSON.parse($response.body);
 
-if (/users\?birth_time/.test($request.url)) {
-    const data = anye.data;
+const data = anye.data;
+const extra = anye.extra;
 
-    for (const key in data) {
-        if (data.hasOwnProperty(key) && data[key].hasOwnProperty('extra') && typeof data[key]['extra'] === 'object') {
-            const extra = data[key]['extra'];
-
-            for (const item of extra) {
-                if (item.hasOwnProperty('adms_user') && Array.isArray(item['adms_user'])) {
-                    item['adms_user'].forEach(ad => {
-                        ad.ranking = 0;
-                        ad.ranking_banner_one = 0;
-                        ad.is_ads = 0;
-                    });
-                }
+function setFieldsToZero(obj) {
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (key === 'ranking' || key === 'ranking_banner_one' || key === 'is_ads') {
+                obj[key] = 0;
+            } else if (typeof obj[key] === 'object') {
+                setFieldsToZero(obj[key]);
             }
         }
     }
-
-    $done({ body: JSON.stringify(anye) }); // 输出处理后的 JSON 数据
-} else {
-    $done({});
 }
+
+setFieldsToZero(data);
+setFieldsToZero(extra);
+
+$done({ body: JSON.stringify(anye) }); // 输出处理后的 JSON 数据
