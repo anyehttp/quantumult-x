c7b0d5b0ad8eda1e67e16f7748c410341202ca6c
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-21 16:58:07 +0800

    更新 sj2.js

--- beta/sj2.js
+++ beta/sj2.js
@@ -20,24 +20,4 @@ const vip1 = /^http:\/\/login\.user\.kugou\.com\/v5\/login_by_token/;
     }
 
 
-
-
-$done({body: JSON.stringify(anye)});
-*************************************/
-
-var anye = JSON.parse($response.body);
-
-const vip1 = /^http:\/\/login\.user\.kugou\.com\/v5\/login_by_token/;
-
-    anye.data = {
-            "vip_token" : "99999999",
-            "vip_end_time" : "9999-99-99 17:32:25",
-            "su_vip_end_time" : "9999-99-99 17:32:25",
-            "is_vip" : 1,
-            "su_vip_clearday" : "9999-99-99 17:32:25",
-    }
-
-
-
-
 $done({body: JSON.stringify(anye)});
