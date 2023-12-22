562384fd1543deb244899d12dc711fded316ca26
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-22 11:59:19 +0800

    更新 5.js

---
+++ beta/5.js
@@ -0,0 +1,13 @@
+/*
+
+[rewrite_local]
+^https:\/\/memoryhelper\.fujianjin6471\.com\/api\/v4\.7\/sts_token url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/5.js
+
+
+[MITM]
+hostname = memoryhelper.fujianjin6471.com
+
+*/
+var anye = JSON.parse($response.body);
+anye.Expiration = "9999-12-22T04:48:59Z";
+$done({ body: JSON.stringify(anye) });
