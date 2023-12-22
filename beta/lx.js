c6de772923b46cb69a7b8c2fd4ba4a2a6969c02b
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-22 19:13:57 +0800

    Create lx.js

---
+++ beta/lx.js
@@ -0,0 +1,10 @@
+/*
+
+^https:\/\/api\.club\.lenovo\.cn\/common\/open_ad  url script-response-body  https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/lx.js
+
+hostname = api.club.lenovo.cn
+
+*/
+var anye = JSON.parse($response.body);
+anye. res =｛"residence_Time" = 0｝
+$done({ body: JSON.stringify(anye) });
