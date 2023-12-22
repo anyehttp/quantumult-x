37b9ecfcc110be824b490498c58d828ff741ad80
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-22 19:16:18 +0800

    Update lx.js

--- beta/lx.js
+++ beta/lx.js
@@ -1,6 +1,6 @@
 /*
 [rewrite_local]
-^https:\/\/api\.club\.lenovo\.cn\/common\/open_ad  url script-response-body  https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/lx.js
+^https:\/\/api\.club\.lenovo\.cn\/common\/open_ad url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/lx.js
 [MITM]
 hostname = api.club.lenovo.cn
 
