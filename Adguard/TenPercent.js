6f32b6d58fb3eb19b3a267f66684de2f24f6a7a6
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-24 02:46:12 +0800

    更新 TenPercent.js

--- Adguard/TenPercent.js
+++ Adguard/TenPercent.js
@@ -20,19 +20,15 @@ hostname = api.changecollective.com
 
 var body = $response.body;
 var anye = JSON.parse(body);
-    anye.user = {
-    "subscription_period" : "P1Y",
-    "created_at" : "2023-12-23T17:57:36Z",
-    "expiration_date" : "12/31/2099, 06:21:58 PM UTC",
-    "subscription_in_trial" : true,
-    "plan_description" : "1 year (Trial)",
-    "completed_trial" : true,
-    "display_name" : "暗夜",
-    "subscription_end_date" : "12/30/2099, 08:21:58 PM UTC",
-    "home_segment" : "Default",
-    "subscription_is_auto_renewing" : true,
-    "first_name" : "暗夜",
-    "subscription_source" : "Apple"
-  }
+    
+    anye.user.subscription_period = "P1Y";
+    anye.user.expiration_date = "12/31/2099, 06:21:58 PM UTC";
+    anye.user.subscription_in_trial = true;
+    anye.user.completed_trial = true;
+    anye.user.display_name = "暗夜";
+    anye.user.subscription_end_date = "12/30/2099, 08:21:58 PM UTC";
+    anye.user.subscription_is_auto_renewing = true;
+    anye.user.first_name = "暗夜";
+    anye.user.subscription_source = "Apple";
 
 $done({body : JSON.stringify(anye)});
