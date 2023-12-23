46af083510ccf768c650406c64bfd68edc49cf53
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-24 03:46:57 +0800

    更新 OKMath.js

---
+++ Adguard/OKMath.js
@@ -0,0 +1,26 @@
+/*************************************
+
+项目名称：TenPercent
+下载地址：https://too.st/71x
+更新日期：2023-12-24
+脚本作者：@anyeyey
+指导: @ios151
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/app\.coding61\.com\/server\/code_orange\/get_user_property url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/OKMath.js 
+
+[mitm]
+hostname = app.coding61.com
+
+*************************************/
+
+
+var anye = JSON.parse($response.body);
+    
+    anye.user_property.remain_data = 99;
+    anye.user_property.exp_date = "2099-01-01T00:00:00";
+
+$done({body : JSON.stringify(anye)});
