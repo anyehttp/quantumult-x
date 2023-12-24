f7b83f752db3a39e83a007fe8010ea92f18fd44e
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-24 13:06:38 +0800

    更新 mdags.js

--- Adguard/mdags.js
+++ Adguard/mdags.js
@@ -10,7 +10,9 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.feihua100\.com\/api\/feihua\/v2\/course\/session\/verifySession url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/mdags.js 
+^https:\/\/api\.feihua100\.com\/api\/feihua\/v4\/course\/radio\/userRadio\? url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/mdags.js 
+^https:\/\/api\.feihua100\.com\/api\/feihua\/v2\/poemAlbum\/chapter\/course\/list\? url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/mdags.js
+^https:\/\/api\.feihua100\.com\/api\/feihua\/v2\/course url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/mdags.js
 
 [mitm]
 hostname = api.feihua100.com
@@ -18,12 +20,21 @@ hostname = api.feihua100.com
 *************************************/
 
 
-var anye = JSON.parse($response.body);
-let array = anye.data;
-    array.map(open => {
-        open.isOpen = 1;
-        open.starVal = 3;
-    });
-    anye.data = array;
+var body = $response.body;
 
-$done({body : JSON.stringify(anye)});
+//关卡
+body = body.replace(/"isOpen"\s*:\s*0/g, '"isOpen": 1');
+
+//畅学包
+body = body.replace(/"isEnable"\s*:\s*2/g, '"isEnable": 1');
+
+//畅学包
+body = body.replace(/"isExperiential"\s*:\s*2/g, '"isExperiential": 1');
+
+//听儿歌
+body = body.replace(/"is_exp_class"\s*:\s*0/g, '"is_exp_class": 1');
+
+//听儿歌
+body = body.replace(/"radio_order"\s*:\s*1/g, '"radio_order": 2');
+
+$done({ body });
