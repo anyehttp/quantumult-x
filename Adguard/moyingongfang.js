b9761425dbfb1a58efb17fb2388bf725903f96ec
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-24 19:31:06 +0800

    更新 moyingongfang.js

---
+++ Adguard/moyingongfang.js
@@ -0,0 +1,32 @@
+/*************************************
+
+项目名称：OKMath全科启蒙
+下载地址：https://too.st/71y
+更新日期：2023-12-24
+脚本作者：@anyeyey
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/moyin-gateway\.moyin\.com\/moyin-account\/v1\/vip\/status url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/moyingongfang.js 
+
+[mitm]
+hostname = moyin-gateway.moyin.com
+
+*************************************/
+
+
+var anye = JSON.parse($response.body);
+    
+    anye.data.leftDays = 9999;
+    anye.data.vipEndTime = 4101794198000;
+    anye.data.endTime = 4101794198000;
+    anye.data.vipLeftDaysForDisplay = 999;
+    anye.data.superEndTime = 4101794198000;
+    anye.data.curSeatCountLeftDays = 999;
+    anye.data.leftDaysForDisplay = 999;
+    anye.data.expireDays = 4101794198000;
+    anye.data.realVip = true;
+    
+$done({body : JSON.stringify(anye)});
