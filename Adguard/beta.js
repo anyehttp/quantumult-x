ff904f101cbce2fcf02b38741b21d0fb38188d9a
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-25 17:10:30 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -10,16 +10,65 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.cikelink\.com\/fm\/v2\/fm\/userInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers\/$RCAnonymousID url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
 
 [mitm]
-hostname = api.cikelink.com
+hostname = api.revenuecat.com
 
 *************************************/
 
 
-var body = $response.body.replace(/status":\d/g,'status":1')
-.replace(/expireTime":\d+/g,'expireTime":4567891456')
-.replace(/expireUnixTime":\d+/g,'expireUnixTime":4567891456')
-.replace(/canPlay":\w+/g,'canPlay":true')
-$done({ body });
+var anye = JSON.parse($response.body);
+
+#var vip1 = /^https:\/\/memoryhelper\.fujianjin6471\.com\/api\/v4\.7\/sts_token/
+#var vip1 = /^https:\/\/memoryhelper\.fujianjin6471\.com\/api\/v4\.7\/expiration_date/
+
+#if(vip1.test($request.url)){
+    anye = {
+  "request_date_ms" : 1703493890218,
+  "request_date" : "2023-12-25T08:44:50Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+      "lifetime" : [
+        {
+          "id" : "cd922147e3",
+          "is_sandbox" : false,
+          "purchase_date" : "2023-12-25T08:43:55Z",
+          "original_purchase_date" : "2023-12-25T08:43:55Z",
+          "store" : "app_store",
+          "store_transaction_id" : "460001746951430"
+        }
+      ]
+    },
+    "first_seen" : "2023-12-25T08:42:59Z",
+    "original_application_version" : "161",
+    "other_purchases" : {
+      "lifetime" : {
+        "purchase_date" : "2023-12-25T08:43:55Z"
+      }
+    },
+    "management_url" : null,
+    "subscriptions" : {
+
+    },
+    "entitlements" : {
+      "pro" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2023-12-25T08:43:55Z",
+        "product_identifier" : "lifetime",
+        "expires_date" : null
+      }
+    },
+    "original_purchase_date" : "2023-12-25T08:42:47Z",
+    "original_app_user_id" : "$RCAnonymousID:39f304851bfb47e490477c29b7a5f1ca",
+    "last_seen" : "2023-12-25T08:42:59Z"
+  }
+};
+
+#}
+
+#if(vip2.test($request.url)){
+    #anye.decrease_subscription_from_server = true;
+    #anye.expiration_date = 9999;
+#}
+$done({ body: JSON.stringify(anye) });
