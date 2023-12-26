46ebfad24918127207eaeafb19833b8ec7b8edad
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-27 03:20:20 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -17,17 +17,33 @@ hostname = d1hzbu30hrhkoe.cloudfront.net
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
+/*************************************
 
-chxm1023.data = {...chxm1023.data,
-    "is_grace" : true,
-    "purchased" : true,
-    "is_trial" : true,
-    "is_cancelled" : true,
-    "type" : 2,
-    "has_orders" : true,
-    "expiration" : 4092599349,
-    "is_expired" : false
-  };
+项目名称：穿梭
+下载地址：https://too.st/72S
+更新日期：2023-12-24
+脚本作者：@anyeyey
+使用声明：⚠️仅供参考，🈲转载与售卖！
+群组：https://t.me/IPAs_share
+**************************************
 
-$done({body : JSON.stringify(chxm1023)});
+[rewrite_local]
+^https:\/\/mbd\.baidu\.com\/userx\/v1\/info\/get url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
+
+[mitm]
+hostname = mbd.baidu.com
+
+*************************************/
+
+
+var anye = JSON.parse($response.body);
+    
+    anye.data.fields.displayname = "暗夜";
+    anye.data.fields.username = "暗夜";
+    anye.data.fields.member = {
+        "is_vip" : 1,
+        "end_time" : 4101995901,
+      };
+    
+
+$done({body : JSON.stringify(anye)});
