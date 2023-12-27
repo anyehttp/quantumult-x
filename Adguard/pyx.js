6a5e190f3dfcad316ed5917cc058e4122620daf1
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-27 15:30:11 +0800

    更新 pyx.js

---
+++ Adguard/pyx.js
@@ -0,0 +1,28 @@
+/*************************************
+
+项目名称：魔音工坊
+下载地址：https://too.st/723
+更新日期：2023-12-24
+脚本作者：@anyeyey
+使用声明：⚠️仅供参考，🈲转载与售卖！
+群组：https://t.me/IPAs_share
+**************************************
+
+[rewrite_local]
+^https:\/\/iosapi\.peiyinxiu\.com\/Api\/user\/GetMyInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/pyx.js 
+
+[mitm]
+hostname = iosapi.peiyinxiu.com
+
+*************************************/
+
+
+var anye = JSON.parse($response.body);
+        anye.data.vip_end_time = 4101794198000;
+        anye.data.gold2 = 99999;
+        anye.data.vipLevel = 9999,
+        anye.data.is_complete_info = 1;
+        anye.data.is_vip = 1;
+        anye.data.user_name = "暗夜";
+        
+$done({body: JSON.stringify(anye)});
