015b30f2810219989bb6b9ae9c13e2acc380aca7
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 04:09:22 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -1,29 +1,61 @@
+/*
 
-/*************************************
-
-项目名称：穿梭
-下载地址：https://too.st/72S
-更新日期：2023-12-24
-脚本作者：@anyeyey
-使用声明：⚠️仅供参考，🈲转载与售卖！
-**************************************
+Persona 1.8.24
 
 [rewrite_local]
-^https:\/\/iosapi\.peiyinxiu\.com\/Api\/Film\/HomeData url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
-^https:\/\/iosapi\.peiyinxiu\.com\/Api\/user\/GetMyInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
-[mitm]
-hostname = iosapi.peiyinxiu.com
 
-*************************************/
-var anye = JSON.parse($response.body);
-const vip1 = /^https:\/\/iosapi\.peiyinxiu\.com\/Api\/Film\/HomeData/;
-const vip2 = /^https:\/\/iosapi\.peiyinxiu\.com\/Api\/user\/GetMyInfo/;
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
 
-    if(vip1.test($request.url)){
-        anye.data.film.is_vip = true;
+[mitm] 
+
+hostname = api.revenuecat.com
+
+*/
+
+var objc = JSON.parse($response.body);
+
+    objc = {
+  "request_date_ms" : 1703880024609,
+  "request_date" : "2023-12-29T20:00:24Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2023-12-29T19:57:06Z",
+    "original_application_version" : "433",
+    "other_purchases" : {
+
+    },
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "com.hello.german.yearly" : {
+        "original_purchase_date" : "2023-12-29T20:00:05Z",
+        "expires_date" : "2099-01-05T20:00:04Z",
+        "is_sandbox" : false,
+        "refunded_at" : null,
+        "store_transaction_id" : "500001586568718",
+        "unsubscribe_detected_at" : null,
+        "grace_period_expires_date" : null,
+        "period_type" : "trial",
+        "purchase_date" : "2023-12-29T20:00:04Z",
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "store" : "app_store",
+        "auto_resume_date" : null
+      }
+    },
+    "entitlements" : {
+      "Premium" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2023-12-29T20:00:04Z",
+        "product_identifier" : "com.hello.german.yearly",
+        "expires_date" : "2099-01-05T20:00:04Z"
+      }
+    },
+    "original_purchase_date" : "2023-12-29T19:56:18Z",
+    "original_app_user_id" : "1a85d709-5a08-41ab-87d7-7c193ca9b66f",
+    "last_seen" : "2023-12-29T19:57:06Z"
+  }
 }
-    if(vip2.test($request.url)){
-        anye.data.is_vip = 1;
-}
 
-$done({body : JSON.stringify(anye)});
+$done({body : JSON.stringify(objc)});
