04a43cd09dd298f19b52d1c02f062c1051e9773c
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 02:59:24 +0800

    更新 Wink.js

---
+++ Adguard/Wink.js
@@ -0,0 +1,31 @@
+/*************************************
+
+项目名称：Wink
+下载地址：https://too.st/7aU
+更新日期：2023-12-30
+脚本作者：@anyeyey
+使用声明：⚠️仅供参考，🈲转载与售卖！
+**************************************
+
+[rewrite_local]
+^https:\/\/api-winkcut\.meitu\.com\/user\/show url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/Wink.js 
+^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/Wink.js
+[mitm]
+hostname = api-winkcut.meitu.com, api-sub.meitu.com
+
+*************************************/
+var anye = JSON.parse($response.body);
+const vip1 = /^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip/;
+const vip2 = /^https:\/\/api-winkcut\.meitu\.com\/user\/show/;
+
+    if(vip1.test($request.url)){
+        anye.data.is_vip = true;
+        anye.data.have_valid_contract = true;
+        anye.data.use_vip = true;
+}
+    if(vip2.test($request.url)){
+        anye.data.vip_type = 1;
+        anye.data.screen_name = "暗夜";
+}
+
+$done({body : JSON.stringify(anye)});
