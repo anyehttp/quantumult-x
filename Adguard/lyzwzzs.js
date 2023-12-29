dce3008ecf8560a4340b5a69ea8286ea9c2df06e
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-29 14:59:27 +0800

    更新 lyzwzzs.js

--- Adguard/lyzwzzs.js
+++ Adguard/lyzwzzs.js
@@ -10,7 +10,7 @@
 
 [rewrite_local]
 ^https:\/\/api\.luyingou\.com\/user\/getAppUser url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/lyzwzzs.js 
-
+^https:\/\/api\.luyingou\.com\/user\/vip\/getIosVipPage url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/lyzwzzs.js
 [mitm]
 hostname = api.luyingou.com
 
@@ -18,11 +18,22 @@ hostname = api.luyingou.com
 
 
 var anye = JSON.parse($response.body);
-    
+const vip1 = /^https:\/\/api\.luyingou\.com\/user\/getAppUser/;
+const vip2 = /^https:\/\/api\.luyingou\.com\/user\/vip\/getIosVipPage/;
+
+if(vip1.test($request.url)){
     anye.data.user.vipExpire = 9999;
     anye.data.user.vipMonthConvertTime = 9999;
     anye.data.user.isVip = true;
     anye.data.user.alias = "暗夜";
     anye.data.user.name = "暗夜";
+}
+
+if(vip2.test($request.url)){
+    anye.data.userVo.vipExpire = 9999;
+    anye.data.userVo.isVip = true;
+    anye.data.userVo.alias = "暗夜";
+    anye.data.userVo.name = "暗夜";
+}
 
 $done({body : JSON.stringify(anye)});
