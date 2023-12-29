4300dbd4c0c889b3af8613c043c765e9a23b727f
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-29 16:22:14 +0800

    更新 Wakeout.js

--- Adguard/Wakeout.js
+++ Adguard/Wakeout.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat.com\/v1\/subscribers\/* url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/Wakeout.js 
+^https:\/\/api\.revenuecat.com\/v1\/subscribers/* url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/Wakeout.js 
 
 [mitm]
 hostname = api.revenuecat.com
