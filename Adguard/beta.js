84ce11b8a3f6a34111bec42be3be2c8a3ac0cc36
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 03:34:25 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -9,24 +9,21 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api-winkcut\.meitu\.com\/user\/show url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
-^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
+^https:\/\/iosapi\.peiyinxiu\.com\/Api\/Film\/HomeData url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
+^https:\/\/iosapi\.peiyinxiu\.com\/Api\/user\/GetMyInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
 [mitm]
-hostname = api-winkcut.meitu.com, api-sub.meitu.com
+hostname = iosapi.peiyinxiu.com
 
 *************************************/
 var anye = JSON.parse($response.body);
-const vip1 = /^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip/;
-const vip2 = /^https:\/\/api-winkcut\.meitu\.com\/user\/show/;
+const vip1 = /^https:\/\/iosapi\.peiyinxiu\.com\/Api\/Film\/HomeData/;
+const vip2 = /^https:\/\/iosapi\.peiyinxiu\.com\/Api\/user\/GetMyInfo/;
 
     if(vip1.test($request.url)){
-        anye.data.is_vip = true;
-        anye.data.have_valid_contract = true;
-        anye.data.use_vip = true;
+        anye.data.film.is_vip = true;
 }
     if(vip2.test($request.url)){
-        anye.data.vip_type = 1;
-        anye.data.screen_name = "暗夜";
+        anye.data.is_vip = 1;
 }
 
 $done({body : JSON.stringify(anye)});
