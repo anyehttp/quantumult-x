7ad6dec88c031e7e6ad616b2853321127cacc182
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 02:54:25 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -9,20 +9,24 @@
 **************************************
 
 [rewrite_local]
-^https://api-(winkcut\.meitu\.com/user/show|sub\.meitu\.com/v2/user/vip)$ url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
-
+^https:\/\/api-winkcut\.meitu\.com\/user\/show url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js 
+^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
 [mitm]
-hostname = api-*.meitu.com
+hostname = api-winkcut.meitu.com, api-sub.meitu.com
 
 *************************************/
-
-
 var anye = JSON.parse($response.body);
+const vip1 = /^https:\/\/api-sub\.meitu\.com\/v2\/user\/vip/;
+const vip2 = /^https:\/\/api-winkcut\.meitu\.com\/user\/show/;
 
-    anye.data.is_vip = true;
-    anye.data.have_valid_contract = true;
-    anye.data.use_vip = true;
-    anye.data.vip_type = 1;
-    anye.data.screen_name = "暗夜";
+    if(vip1.test($request.url)){
+        anye.data.is_vip = true;
+        anye.data.have_valid_contract = true;
+        anye.data.use_vip = true;
+}
+    if(vip2.test($request.url)){
+        anye.data.vip_type = 1;
+        anye.data.screen_name = "暗夜";
+}
 
 $done({body : JSON.stringify(anye)});
