81db9ccd5e59e40432a5dfb8f38e89e4e0a9d693
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 19:59:24 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -1,15 +1,4 @@
 /*
-if(vip1.test($request.url))
-
-var anye = JSON.parse($response.body);
-for (var i = 0; i < anye.obj.buyLst.length; i++) {
-    anye.obj.buyLst[i].payAmount = "0";
-}
-$done({body : JSON.stringify(anye)}); 
-
-
-Persona 1.8.24
-
 [rewrite_local]
 
 ^https:\/\/www\.doctorsclass\.com\/api\/v1\.wechatUser\/getUserInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
