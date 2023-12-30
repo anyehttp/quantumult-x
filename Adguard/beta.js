22018bf4eb68a6ca9a988b074c898ed825e6dcbe
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 22:25:24 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -1,23 +1,16 @@
 /*
+headers['MConfig-Info'] = '';
+****************************
 [rewrite_local]
-
-^https:\/\/www\.doctorsclass\.com\/api\/v1\.wechatUser\/getUserInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
+^https:\/\/www\.doctorsclass\.com\/api\/v1 url script-request-header https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
 
 [mitm] 
-
 hostname = doctorsclass.com
-
 */
+headers = $request.headers;
 
-var anye = JSON.parse($response.body);
+headers['Cookie'] = 'dMYGUvJAeBkbU7WgxUyDrKj+gjkVqqJH8c73Po1sj9ktkKmobz/NVsfSJ3C06Icv';
+headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Html5Plus/1.0 (Immersed/20) uni-app';
 
-    anye.vip_expire = 1;
-    anye.nick_name = "暗夜";
-    anye.vip = {
-    "id": "4",
-    "expire": "2099-12-29",
-    "start": "2023-12-29"
-  };
-    anye.vip_id = 1;
-    
-$done({body : JSON.stringify(anye)}); 
+
+$done({headers});
