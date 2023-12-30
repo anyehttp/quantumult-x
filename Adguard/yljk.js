afcb37dcd823ac367c87c9972d4dd4aadc365df3
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 16:54:11 +0800

    更新 yljk.js

---
+++ Adguard/yljk.js
@@ -0,0 +1,21 @@
+/*
+
+Persona 1.8.24
+
+[rewrite_local]
+
+^http:\/\/101\.201\.146\.165:8088\/App-EH\/app\/phone\.do\?getCourseDetail_new url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/yljk.js
+
+[mitm] 
+
+hostname = 101.201.146.165:8088
+
+*/
+
+
+
+var anye = JSON.parse($response.body);
+for (var i = 0; i < anye.obj.buyLst.length; i++) {
+    anye.obj.buyLst[i].payAmount = "0";
+}
+$done({body : JSON.stringify(anye)}); 
