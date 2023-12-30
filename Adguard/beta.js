34295e5d960d0a1f26042c6eaaca126e8720f969
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 19:42:52 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -1,21 +1,30 @@
 /*
-
-Persona 1.8.24
-
-[rewrite_local]
-
-^http:\/\/101\.201\.146\.165:8088\/App-EH\/app\/phone\.do\?getCourseDetail_new url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
-
-[mitm] 
-
-hostname = 101.201.146.165:8088
-
-*/
-
-
+if(vip1.test($request.url))
 
 var anye = JSON.parse($response.body);
 for (var i = 0; i < anye.obj.buyLst.length; i++) {
     anye.obj.buyLst[i].payAmount = "0";
 }
+$done({body : JSON.stringify(anye)}); 
+
+
+Persona 1.8.24
+
+[rewrite_local]
+
+^https:\/\/api\.maoyan\.com\/sns\/user\/1873536104\/profile url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
+
+[mitm] 
+
+hostname = api.maoyan.com
+
+*/
+
+
+var anye = JSON.parse($response.body);
+    anye.data.user.currentExp = 12900;
+    anye.data.user.vipType = 1;
+    anye.data.user.totalExp = 0;
+    anye.data.user.username = "暗夜";
+    
 $done({body : JSON.stringify(anye)}); 
