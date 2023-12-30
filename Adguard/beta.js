c282988de1c6fb391f0b188d5c30c14ff011eaa2
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-30 19:58:16 +0800

    更新 beta.js

--- Adguard/beta.js
+++ Adguard/beta.js
@@ -12,19 +12,20 @@ Persona 1.8.24
 
 [rewrite_local]
 
-^https:\/\/api\.maoyan\.com\/sns\/user\/1873536104\/profile url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
+^https:\/\/www\.doctorsclass\.com\/api\/v1\.wechatUser\/getUserInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/beta.js
 
 [mitm] 
 
-hostname = api.maoyan.com
+hostname = doctorsclass.com
 
 */
 
-
 var anye = JSON.parse($response.body);
-    anye.data.user.currentExp = 12900;
-    anye.data.user.vipType = 1;
-    anye.data.user.totalExp = 0;
-    anye.data.user.username = "暗夜";
+
+    anye.vip_expire = 1;
+    anye.update_time = "2099-12-30 19:48:13";
+    anye.nick_name = "暗夜";
+    anye.vip = 4102314836;
+    anye.vip_id = 1;
     
 $done({body : JSON.stringify(anye)}); 
