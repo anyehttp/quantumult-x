e922eadecb1c280835ffcf40f6574afed2f0b9a4
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-31 00:02:32 +0800

    Create yk.js

---
+++ Adguard/yk.js
@@ -0,0 +1,20 @@
+/*****************************
+
+[rewrite_local]
+^https:\/\/www\.doctorsclass\.com\/api\/v1\.index\/subscribed url script-request-header https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/yk.js
+^https:\/\/www\.doctorsclass\.com\/api\/v1\.index\/banner url script-request-header https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/yk.js
+^https:\/\/www\.doctorsclass\.com\/api\/v1\.category\/homeCate url script-request-header https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/yk.js
+^https:\/\/www\.doctorsclass\.com\/api\/v1\.index\/index url script-request-header https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/yk.js
+^https:\/\/www\.doctorsclass\.com\/api\/v1\.category\/all url script-request-header https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/yk.js
+^https:\/\/www\.doctorsclass\.com\/api\/v1\.wechatUser\/getUserInfo url script-request-header https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/yk.js
+
+[mitm] 
+hostname = www.doctorsclass.com
+********************************/
+var headers = $request.headers;
+
+headers['Cookie'] = 'dMYGUvJAeBkbU7WgxUyDrKj+gjkVqqJH8c73Po1sj9ktkKmobz/NVsfSJ3C06Icv';
+headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Html5Plus/1.0 (Immersed/20) uni-app';
+headers['token'] = 'XSUWbGZvVLLd23cO5+BwLQRlmi+ECYQHDumM6XYhgbv24l/dpQFM29I9Bx66c0NG';
+
+$done({headers});
