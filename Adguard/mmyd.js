db350e12b922974249d1bb54e590c5a137eb3913
anyehttp <93598563+anyehttp@users.noreply.github.com>
2023-12-31 23:10:16 +0800

    更新 mmyd.js

---
+++ Adguard/mmyd.js
@@ -0,0 +1,27 @@
+/*************************************
+
+项目名称：喵喵阅读
+下载地址：https://t.cn/R9CIJLb
+更新日期：2023-12-31
+脚本作者：暗夜
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/api\.miaoread\.net\/book\/config\/getAdsSettings url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/mmyd.js
+
+[mitm]
+hostname = api.miaoread.net
+*************************************/
+
+
+var anye = JSON.parse($response.body);
+
+    anye.result.readRewardTime = 9999;
+    anye.result.listenFirstTime = 9999;
+    anye.result.listenRewardTime = 9999;
+    anye.result.useDownloadReward = true;
+
+
+$done({body: JSON.stringify(anye)});
