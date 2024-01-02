b0d4aa08a7413973e6822ed741849f61631ae082
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-02 16:13:49 +0800

    更新 speak.js

--- Adguard/speak.js
+++ Adguard/speak.js
@@ -20,5 +20,5 @@ var anye = JSON.parse($response.body);
 	anye.info.premium = true;
 	anye.info.joinDate = "1924-01-02T07:09:02.000Z";
 	//anye.info.progress.todaySentencesCount.lastRecordingDate = "1924-01-02T07:10:47.259Z";
-	anye.info.name = "暗夜";
+	anye.info.name = "暗夜11";
 $done({body: JSON.stringify(anye)});
