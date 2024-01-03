2efbd2e907c0d134f3827b85d4fc04f0fa142a11
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-04 01:03:49 +0800

    更新 zmm.js

--- Adguard/zmm.js
+++ Adguard/zmm.js
@@ -10,10 +10,23 @@
 
 [rewrite_local]
 ^https:\/\/gs\.gateway\.gameley\.cn\/np-user\/user\/coin\/info url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/zmm.js
+^https:\/\/gs\.gateway\.gameley\.cn\/np-user\/shopping\/buy url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/zmm.js
 [mitm]
 hostname = gs.gateway.gameley.cn
 *************************************/
 var anye = JSON.parse($response.body);
+const vip1 = /^https:\/\/gs\.gateway\.gameley\.cn\/np-user\/user\/coin\/info/;
+const vip2 = /^https:\/\/gs\.gateway\.gameley\.cn\/np-user\/shopping\/buy/;
+
+
+if(vip1.test($request.url)){
     anye.info.coinNumber = 9999;
+}
+
+if(vip2.test($request.url)){
+    anye.info.number = 999;
+    anye.info.status.isNeeds = true;
+    anye.info.status.isNeeds = true;
+}
 
 $done({body : JSON.stringify(anye)});
