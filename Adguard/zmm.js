15270cf3d403ed6deaa0ce4e512917f1630d9bd8
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-03 23:24:52 +0800

    更新 zmm.js

---
+++ Adguard/zmm.js
@@ -0,0 +1,26 @@
+/*************************************
+
+项目名称：桌面猫
+下载地址：https://too.st/7dn
+更新日期：2024-01-03
+脚本作者：@anyeyey
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/gs\.gateway\.gameley\.cn\/np-user\/shopping\/list url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/zmm.js
+[mitm]
+hostname = gs.gateway.gameley.cn
+*************************************/
+var anye = JSON.parse($response.body);
+
+        if (anye.hasOwnProperty('daily')) {
+            for (let i = 0; i < anye.daily.length; i++) {
+                if (anye.daily[i].hasOwnProperty('originalPrice')) {
+                    anye.daily[i].originalPrice = 0;
+                }
+            }
+        }
+        
+$done({body: JSON.stringify(obj)});
