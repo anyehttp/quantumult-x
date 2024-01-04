7b8091a6a2e8c5bff5bb8ca2adc593739c315bf2
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-05 06:22:58 +0800

    更新 soul.js

--- Adguard/soul.js
+++ Adguard/soul.js
@@ -7,14 +7,14 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo\?bi url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
-^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf\?bi url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
+^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
+^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
 [mitm]
 hostname = api-a.soulapp.cn
 *************************************/
 var anye = JSON.parse($response.body);
-const vip1 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo\?bi/;
-const vip2 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf\?bi/;
+const vip1 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo/;
+const vip2 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf/;
 
 if(vip1.test($request.url)){
     anye.data.limitType = 1;
