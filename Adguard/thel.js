878455412ec3e77a5430a05c19518f817744b9d4
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-04 21:46:52 +0800

    更新 thel.js

--- Adguard/thel.js
+++ Adguard/thel.js
@@ -12,17 +12,16 @@
 ^https:\/\/api\.rela\.me\/v3\/interface\/stay\/myInfoForFriend url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/thel.js
 ^https:\/\/api\.rela\.me\/v3\/payment\/ios-vip-status url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/thel.js
 ^https:\/\/api\.rela\.me\/v3\/users\/vip\/detail url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/thel.js
+^https:\/\/api\.rela\.me\/v2\/params\/init\?apptype url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/thel.js
 [mitm]
 hostname = api.rela.me
-
-
 *************************************/
 
 var anye = JSON.parse($response.body);
 const vip1 = /^https:\/\/api\.rela\.me\/v3\/interface\/stay\/myInfoForFriend/;
 const vip2 = /^https:\/\/api\.rela\.me\/v3\/payment\/ios-vip-status/;
 const vip3 = /^https:\/\/api\.rela\.me\/v3\/users\/vip\/detail/;
-
+const vip4 = /^https:\/\/api\.rela\.me\/v2\/params\/init\?apptype/;
 if(vip1.test($request.url)){
     anye.data.nickName = "1暗夜1";
     anye.data.vipType = 1;
@@ -40,9 +39,43 @@ if(vip2.test($request.url)){
   };
 }
 if(vip3.test($request.url)){
-    anye.data.vipSetting.expireTime = "2099-01-01";
-    anye.data.vipSetting.endTimestamp = 4071216264;
-    anye.data.vipSetting.startTimestamp = 1704374665;
-    anye.data.vipSetting.vipType = 1;
+    anye.data.vipSetting = {
+      "expireTime" : "2099-01-01",
+      "distanceHiding" : 1,
+      "followRemind" : 1,
+      "endTimestamp" : 4071216264,
+      "autoSubscribe" : 1,
+      "winkHiding" : 1,
+      "startTimestamp" : 1704374665,
+      "userId" : 0,
+      "level" : 99,
+      "incognito" : 1,
+      "vipHiding" : 1,
+      "onlineHiding" : 1,
+      "fansHiding" : 1,
+      "hiding" : 1,
+      "vipType" : 1,
+      "liveHiding" : 1,
+      "vipIconLevel" : 1,
+      "msgHiding" : 1
+    };
 }
+if(vip4.test($request.url)){
+  anye.data.vipSetting = {
+      "hiding" : 1,
+      "vipHiding" : 1,
+      "onlineHiding" : 1,
+      "winkHiding" : 1,
+      "incognito" : 1,
+      "liveHiding" : 1,
+      "msgHiding" : 1,
+      "fansHiding" : 1,
+      "followRemind" : 1,
+      "distanceHiding" : 1
+    };
+  anye.data.useFiveSDK = false;
+  anye.data.vipType = 1;
+}
+
+
 $done({ body: JSON.stringify(anye) });
