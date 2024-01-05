ba9fecdab25dcd9dc7c9e509109719d6f1ad0439
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-05 10:30:39 +0800

    更新 zyzx.js

--- Adguard/zyzx.js
+++ Adguard/zyzx.js
@@ -12,6 +12,7 @@
 ^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/loginWithToken url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/zyzx.js
 ^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/getUserCenterInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/zyzx.js
 ^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/getApprovalInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/zyzx.js
+^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/getUserCoin url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/zyzx.js
 [mitm] 
 hostname = mmm.tcmmooc.com
 *************************************/
@@ -19,6 +20,7 @@ var anye = JSON.parse($response.body);
 const vip1 = /^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/loginWithToken/;
 const vip2 = /^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/getUserCenterInfo/;
 const vip3 = /^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/getApprovalInfo/;
+const vip4 = /^http:\/\/mmm\.tcmmooc\.com\/mapi_v3\/User\/getUserCoin/;
 if (vip1.test($request.url)) {
     anye.data.user.vipExpireDate = "4071261955";
     anye.data.user.nickname = "暗夜11";
@@ -31,5 +33,8 @@ if (vip2.test($request.url)) {
 if (vip3.test($request.url)) {
     anye.data.truename = "暗夜";
     anye.data.vip = 1;
+}
+if (vip4.test($request.url)) {
+    anye.data.coin = "9999.99";
 }
 $done({body: JSON.stringify(anye)});
