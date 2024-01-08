fec1998a473ec61095c44860a566f9c17d767c8f
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-09 00:10:01 +0800

    更新 iyf.js

--- Adguard/iyf.js
+++ Adguard/iyf.js
@@ -1,5 +1,5 @@
 /*
-^https:\/\/api\.iyf\.tv\/api\/Video\/GetAdsDataList\?typeList=1,2,3,4,5,6,8&mediaKey=i6qZq1HKc47&System=h5&AppVersion=1\.0&SystemVersion=h5&version=H3&DeviceId url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/iyf.js
+^https:\/\/api\.iyf\.tv\/api\/Video\/GetAdsDataList\?typeList url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/iyf.js
 
 hostname = api.iyf.tv
 */
