c3a7925a5527f3fc4026b2ace6314ce39ec40738
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-09 01:17:34 +0800

    更新 iyf.js

--- Adguard/iyf.js
+++ Adguard/iyf.js
@@ -8,5 +8,6 @@ hostname = api.iyf.tv
 anye = JSON.parse($response.body);
     anye.data.bofang[0].showURL = "";
     anye.data.bofang[0].piDuration = 0;
-    anye.data.bofang[0].chaBo[0].piDuration = 0;
+    anye.data.bofang[0].chaBo[0].piDuration = 0.01;
+    anye.data.bofang[0].chaBo[0].showURL = "https://files.catbox.moe/dcwmvg.MOV";
 $done({body: JSON.stringify(anye)});
