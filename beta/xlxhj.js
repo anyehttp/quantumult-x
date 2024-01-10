e3044bc17fa8b70aed3e04c9f865c39efad1aa16
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-10 17:22:18 +0800

    更新 xlxhj.js

---
+++ beta/xlxhj.js
@@ -0,0 +1,6 @@
+[Script]
+https?:\/\/[\w.]+\/videos3\/[a-f0-9]+\/[a-f0-9]+\.m3u8 url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/m3u8.js
+
+
+[MITM]
+hostname = *.xgfipub.cn
