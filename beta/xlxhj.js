79b344b6c232d62f248992e9c710c15ae4739b07
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-10 23:20:51 +0800

    更新 xlxhj.js

--- beta/xlxhj.js
+++ beta/xlxhj.js
@@ -2,7 +2,11 @@
 #!desc=xl
 
 [MITM]
-hostname = %APPEND% *.xgfipub.cn
+hostname = %APPEND% *.xgfipub.cn, *yangyang4.cn
 
 [Script]
+#gv系列 (GTV Blued视频 小蓝视频)  =*.xgfipub.cn
 xl = type=http-request, pattern=https?:\/\/[\w.]+\/videos3\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
+
+#海角社区  =*yangyang4.cn
+xl = type=http-request, pattern=https?:\/\/[\w.]+\/watch7\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
