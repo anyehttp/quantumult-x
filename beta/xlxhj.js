34997cf3a533007d1c7e3171826da203cfc1bc07
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-11 03:32:22 +0800

    更新 xlxhj.js

--- beta/xlxhj.js
+++ beta/xlxhj.js
@@ -14,8 +14,7 @@ hostname = %APPEND% *.xgfipub.cn, *yangyang4.cn
 
 [Script]
 #gv系列 (GTV Blued视频 小蓝视频)  =*.xgfipub.cn
-xl = type=http-request, pattern=https?:\/\/[\w.]+\/videos3\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
+xl = type=http-request, pattern=https?:\/\/.*\.xgfipub\.cn\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
 
 #海角社区  =*yangyang4.cn
-xl = type=http-request, pattern=https?:\/\/[\w.]+\/static\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-xl = type=http-request, pattern=https?:\/\/[\w.]+\/watch9\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
+xl = type=http-request, pattern=https?:\/\/.*\.yangyang4\.cn\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
