9f6f389d5c39fc8c21672f283c25274d03910209
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-10 18:11:40 +0800

    更新 xlxhj.js

--- beta/xlxhj.js
+++ beta/xlxhj.js
@@ -1,6 +1,8 @@
-[Script]
-https?:\/\/[\w.]+\/videos3\/[a-f0-9]+\/[a-f0-9]+\.m3u8 url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/m3u8.js
-
+#!name=xl
+#!desc=xl
 
 [MITM]
-hostname = *.xgfipub.cn
+hostname = %APPEND% *.xgfipub.cn
+
+[Script]
+xl = type=http-request, pattern=https?:\/\/[\w.]+\/videos3\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
