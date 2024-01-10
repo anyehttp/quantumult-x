95ab3109d06fa2e372b3cb7ac9395f1afb71ccbd
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-11 03:02:10 +0800

    更新 xlxhj.js

--- beta/xlxhj.js
+++ beta/xlxhj.js
@@ -17,4 +17,4 @@ hostname = %APPEND% *.xgfipub.cn, *yangyang4.cn
 xl = type=http-request, pattern=https?:\/\/[\w.]+\/videos3\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
 
 #海角社区  =*yangyang4.cn
-xl = type=http-request, pattern=https?:\/\/[\w.]+\/watch7\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
+xl = type=http-request, pattern=https?:\/\/[\w.]+\/static\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
