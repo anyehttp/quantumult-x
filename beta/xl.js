a20949eb632152aabfae46663eed1f26f5c520fa
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-10 17:44:54 +0800

    更新 xl.js

--- beta/xl.js
+++ beta/xl.js
@@ -1,3 +1,10 @@
+/***
+@anyeyey
+@ios151
+
+小蓝视频网址:https://p4.gv009.fun/
+***/
+
 [rewrite_local]
 https?:\/\/[\w.]+\/videos3\/[a-f0-9]+\/[a-f0-9]+\.m3u8 url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/m3u8.js
 
