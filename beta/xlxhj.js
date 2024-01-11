30f3cbc18ca315915f434d1d04dda585154d251e
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-11 15:46:44 +0800

    更新 xlxhj.js

--- beta/xlxhj.js
+++ beta/xlxhj.js
@@ -4,10 +4,51 @@
 #👑@anyeyey
 #👑@ios151
 
+#失效请反馈
+#群组：https://t.me/IPAs_Dd
+#频道：https://t.me/IPAs_share
+
 #小蓝视频网址:https://p4.gv009.fun/
 #GTV :https://bda.qgtexa.com
 #blued视频:blued.tips
-#海角社区:http://haijiao.fun
+#缅北禁地 https://ac9d.wykfnp.com 
+#暗网解密 https://714.ndqvyg.com 
+#51吃瓜Pro https://3d4f.knjbzw.com 
+#51猎奇 https://088f.bjzezy.com 
+#私房ktv https://89.rwhmcn.com 
+#天涯社区 https://f3d.wchgdn.com 
+#黑洞社区 https://ca64.mdplzw.com 
+#暗网禁区 https://0b.yrhwse.com 
+#她趣 https://5e7.rtlzhe.com 
+#妹团 https://36.paxvrh.com 
+#杏吧 https://9923.hzbwia.com 
+#老九品茶 https://0a.obntac.com 
+#微密圈 https://a4.henwap.com 
+#海角乱伦社区 https://f58d.ipqjlb.com 
+#51动漫 https://75.luduwz.com 
+#蜜豆视频 https://6de.scrrzr.com 
+#黑料天堂 https://42fa.jaedlj.com 
+#51吃瓜 https://lz7as.zzttja.com 
+#新版滴滴 https://cf.sjlqjh.com 
+#七度少女 https://11a.uvmtsn.com
+#91妻友 https://08c0.lvxpha.com 
+#91制片厂Pro https://baf.dhlwyz.com 
+#抖阴Proj https://6f7s.pbgrzh.com 
+#果冻传媒APP https://07.npcwwc.com 
+#福利姬 https://9f.gectab.com 
+#91fans https://fc.zlnkbn.com 
+#pilipili(二次元) https://747.jkhpmy.com 
+#歪歪视频 https://c8.qznpyr.com 
+#91视频-全能版 https://fbrg.rzvwcq.com 
+#撸先生 https://01f4.czhxnw.com 
+#成人B站 https://d4.qhzvgn.com 
+#50度灰 https://od5em.smpaqa.com 
+#51品茶 https://f5f.vavkyj.com 
+#蚂蚁翻墙 https://636d.obqyyh.com 
+#91短视频 https://66.kccber.com 
+#91AV https://ca7.rahzss.com 
+#快手 https://eb81.nnjzpw.com 
+#汤头条 https://a3ou.cxuvtk.com
 
 
 
