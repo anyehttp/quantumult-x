9340abf95b244122879bef64fdf28b79158fd79a
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-12 01:03:06 +0800

    更新 xlxhj.js

--- beta/xlxhj.js
+++ beta/xlxhj.js
@@ -53,122 +53,12 @@
 
 
 [MITM]
-hostname = %APPEND% *.xgfipub.cn, *.wykfnp.com, *.ndqvyg.com, *.knjbzw.com, *.bjzezy.com, *.rwhmcn.com, *.wchgdn.com, *.mdplzw.com, *.yrhwse.com, *.rtlzhe.com, *.paxvrh.com, *.hzbwia.com, *.obntac.com, *.henwap.com, *.yangyang4.cn, *.luduwz.com, *.scrrzr.com, *.jaedlj.com, *.zzttja.com, *.sjlqjh.com, *.uvmtsn.com, *.lvxpha.com, *.dhlwyz.com, *.pbgrzh.com, *.npcwwc.com, *.gectab.com, *.zlnkbn.com, *.jkhpmy.com, *.qznpyr.com, *.rzvwcq.com, *.czhxnw.com, *.qhzvgn.com, *.smpaqa.com, *.vavkyj.com, *.obqyyh.com, *.kccber.com, *.rahzss.com, *.nnjzpw.com, *.cxuvtk.com
+hostname = %APPEND% *
+[Script]
+#!name=ghs
+#!desc=ghs
 
 [Script]
-#gv系列 (GTV Blued视频 小蓝视频)  =*.xgfipub.cn
-xl = type=http-request, pattern=https?:\/\/.*\.xgfipub\.cn\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
+ghs = type=http-request, pattern=https?:\/\/.*\.*\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/ghs.js, timeout=60
 
-#缅北禁地 =*.wykfnp.com 
-xl = type=http-request, pattern=https?:\/\/.*\.wykfnp\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#暗网解密 = *.ndqvyg.com
-xl = type=http-request, pattern=https?:\/\/.*\.ndqvyg\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#51吃瓜Pro = *.knjbzw.com
-xl = type=http-request, pattern=https?:\/\/.*\.knjbzw\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#51猎奇  =*.bjzezy.com
-xl = type=http-request, pattern=https?:\/\/.*\.bjzezy\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#私房ktv   =*.rwhmcn.com 
-xl = type=http-request, pattern=https?:\/\/.*\.rwhmcn\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#天涯社区 =*.wchgdn.com
-xl = type=http-request, pattern=https?:\/\/.*\.wchgdn\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#黑洞社区  =*.mdplzw.com
-xl = type=http-request, pattern=https?:\/\/.*\.mdplzw\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#暗网禁区 =*.yrhwse.com
-xl = type=http-request, pattern=https?:\/\/.*\.yrhwse\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#她趣 =*.rtlzhe.com
-xl = type=http-request, pattern=https?:\/\/.*\.rtlzhe\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#妹团 =*.paxvrh.com
-xl = type=http-request, pattern=https?:\/\/.*\.paxvrh\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#杏吧 =*.hzbwia.com
-xl = type=http-request, pattern=https?:\/\/.*\.hzbwia\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#老九品茶=*.obntac.com
-xl = type=http-request, pattern=https?:\/\/.*\.obntac\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#微密圈 =*.henwap.com
-xl = type=http-request, pattern=https?:\/\/.*\.henwap\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#海角社区  =*.yangyang4.cn
-xl = type=http-request, pattern=https?:\/\/.*\.yangyang4\.cn\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#51动漫 =*.luduwz.com
-xl = type=http-request, pattern=https?:\/\/.*\.luduwz\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#蜜豆视频=*.scrrzr.com
-xl = type=http-request, pattern=https?:\/\/.*\.scrrzr\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#黑料天堂=*.jaedlj.com
-xl = type=http-request, pattern=https?:\/\/.*\.jaedlj\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#51吃瓜=*.zzttja.com
-xl = type=http-request, pattern=https?:\/\/.*\.zzttja\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#新版滴滴 =*.sjlqjh.com
-xl = type=http-request, pattern=https?:\/\/.*\.sjlqjh\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#七度少女=*.uvmtsn.com
-xl = type=http-request, pattern=https?:\/\/.*\.uvmtsn\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#91妻友 =*.lvxpha.com
-xl = type=http-request, pattern=https?:\/\/.*\.lvxpha\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#91制片厂Pro =*.dhlwyz.com
-xl = type=http-request, pattern=https?:\/\/.*\.dhlwyz\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#抖阴Pro  =*.pbgrzh.com
-xl = type=http-request, pattern=https?:\/\/.*\.pbgrzh\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#果冻传媒APP =*.npcwwc.com
-xl = type=http-request, pattern=https?:\/\/.*\.npcwwc\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#福利姬 =*.gectab.com
-xl = type=http-request, pattern=https?:\/\/.*\.gectab\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#91fans =*.zlnkbn.com
-xl = type=http-request, pattern=https?:\/\/.*\.zlnkbn\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#pilipili(二次元) =*.jkhpmy.com
-xl = type=http-request, pattern=https?:\/\/.*\.jkhpmy\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#歪歪视频 =*.qznpyr.com
-xl = type=http-request, pattern=https?:\/\/.*\.qznpyr\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#91视频-全能版 =*.rzvwcq.com
-xl = type=http-request, pattern=https?:\/\/.*\.rzvwcq\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#撸先生 =*.czhxnw.com
-xl = type=http-request, pattern=https?:\/\/.*\.czhxnw\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#成人B站 =*.qhzvgn.com
-xl = type=http-request, pattern=https?:\/\/.*\.qhzvgn\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#50度灰 =*.smpaqa.com
-xl = type=http-request, pattern=https?:\/\/.*\.smpaqa\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#51品茶 =*.vavkyj.com
-xl = type=http-request, pattern=https?:\/\/.*\.vavkyj\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#蚂蚁翻墙 =*.obqyyh.com
-xl = type=http-request, pattern=https?:\/\/.*\.obqyyh\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#91短视频 =*.kccber.com
-xl = type=http-request, pattern=https?:\/\/.*\.kccber\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#91AV =*.rahzss.com
-xl = type=http-request, pattern=https?:\/\/.*\.rahzss\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#快手 =*.nnjzpw.com
-xl = type=http-request, pattern=https?:\/\/.*\.nnjzpw\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
-
-#汤头条 =*.cxuvtk.com
-xl = type=http-request, pattern=https?:\/\/.*\.cxuvtk\.com\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/xl.js, timeout=60
+ghs = type=http-request, pattern=https?:\/\/.*\.*\.cn\/.*\/[a-f0-9]+\/[a-f0-9]+\.m3u8, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/ghs.js, timeout=60
