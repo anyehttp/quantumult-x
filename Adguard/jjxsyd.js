a8f37eaa562af8a3f1a341ca7e78bbe1125ad26b
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-13 01:46:06 +0800

    更新 jjxsyd.js

--- Adguard/jjxsyd.js
+++ Adguard/jjxsyd.js
@@ -7,11 +7,11 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/ios\.jjwxc\.com\/app\.jjwxc\/ios\/reading\/WapAdvertises\/getAd url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/jjxsyd.js
+^https:\/\/ios\.jjwxc\.com\/app\.jjwxc\/ios\/reading\/WapAdvertises\/getAd? url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/jjxsyd.js
 
 [mitm]
 hostname = ios.jjwxc.com
 *************************************/
 var anye = JSON.parse($response.body);
-    anye.data = [{}];
+    anye.data = [];
 $done({body: JSON.stringify(anye)});
