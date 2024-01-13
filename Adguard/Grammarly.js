df289bc0630b5676ed013423ffbd569b80fbdffc
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-14 02:19:18 +0800

    更新 Grammarly.js

---
+++ Adguard/Grammarly.js
@@ -0,0 +1,18 @@
+/*************************************
+项目名称：Grammarly
+下载地址：
+更新日期：2024-01-14
+脚本作者：@anyeyey
+使用声明：⚠️仅供参考，🈲转载与售卖！
+**************************************
+
+[rewrite_local]
+^https:\/\/subscription\.grammarly\.com\/api\/v1\/subscription url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/Grammarly.js
+
+[mitm]
+hostname = subscription.grammarly.com
+*************************************/
+var anye = JSON.parse($response.body);
+    anye.isPremium = true;
+    anye.isAppleSubscription = true;
+$done({body: JSON.stringify(anye)});
