1915574e6accc646b4b9e879efdb7cdc963fd9b2
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-13 23:10:03 +0800

    更新 blued.js

---
+++ beta/blued.js
@@ -0,0 +1,19 @@
+/*************************************
+项目名称：blued
+下载地址：
+更新日期：2024-01-13
+脚本作者：@anyeyey
+使用声明：⚠️仅供参考，🈲转载与售卖！
+**************************************
+
+[rewrite_local]
+^https:\/\/mi\.gdt\.qq\.com\/gdt_mview\.fcg\?posid url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/beta/blued.js
+
+[mitm]
+hostname = mi.gdt.qq.com
+*************************************/
+
+
+var anye = JSON.parse($response.body);
+    anye.data = {};
+$done({body: JSON.stringify(anye)});
