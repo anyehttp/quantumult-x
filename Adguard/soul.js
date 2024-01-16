b417fa55d5c64f53f80a1883755c6aef8aab1d05
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-16 20:22:21 +0800

    更新 soul.js

--- Adguard/soul.js
+++ Adguard/soul.js
@@ -9,22 +9,22 @@
 **************************************
 
 [rewrite_local]
-#^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
+^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
 ^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
 [mitm]
 hostname = api-a.soulapp.cn
 *************************************/
-var anye = JSON.parse($response.body);
-//const vip1 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo/;
-//const vip2 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf/;
 /*
+var anye = JSON.parse($response.body);
+const vip1 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo/;
+const vip2 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf/;
+
 if(vip1.test($request.url)){
     //anye.data.limitType = 1;
     anye.data.speedUp = true;
-}
-if(vip2.test($request.url)){
-*/
+};
 
+if(vip2.test($request.url)){
     //anye.data.limitType = 1;
     //速度?
     anye.data.currentSpeed = 100;
@@ -34,5 +34,27 @@ if(vip2.test($request.url)){
     anye.data.speedupCount = 9999;
     //加速否
     anye.data.speedup = true;
+};
+$done({ body: JSON.stringify(anye) });
+*/
+
+var anye = JSON.parse($response.body);
+const vip1 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo/;
+const vip2 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf/;
+
+if (anye.data) {
+    if (vip1.test($request.url)) {
+        anye.data.speedUp = true;
+    }
+
+    if (vip2.test($request.url)) {
+        anye.data.currentSpeed = 100;
+        anye.data.speedupTime = 9999;
+        anye.data.speedupCount = 9999;
+        anye.data.speedup = true;
+    }
+} else {
+    console.error("anye.data 不可用或结构意外。");
+}
 
 $done({ body: JSON.stringify(anye) });
