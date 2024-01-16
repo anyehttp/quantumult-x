83b8e7b063004989537e5455d48316c6c0dc0154
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-16 09:40:11 +0800

    更新 fk.js

--- Adguard/fk.js
+++ Adguard/fk.js
@@ -9,31 +9,16 @@
 
 [rewrite_local]
 ^https:\/\/api\.finka\.cn\/v1\/user\/setting\/privacy\?_t url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/fk.js
-^https:\/\/api\.finka\.cn\/user\/profile\/view\/v3\?_t url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/fk.js
 [mitm]
 hostname = api.finka.cn
 *************************************/
 var anye = JSON.parse($response.body);
-const vip1 = /^https:\/\/api\.finka\.cn\/v1\/user\/setting\/privacy\?_t/;
-const vip2 = /^https:\/\/api\.finka\.cn\/user\/profile\/view\/v3\?_t/;
-if(vip1.test($request.url)){
+
     anye.data.hiddenActive = true;
     anye.data.stealthMessage = true;
     anye.data.advertising = false;
     anye.data.hiddenFootmark = true;
     anye.data.hidden = true;
     anye.data.footmarkDelPerMonth = 99999;
-    };
-if(vip2.test($request.url)){
-    anye.data.user.annualSvip = false;
-    anye.data.user.vipAd = 1;
-    anye.data.user.vip = true;
-    anye.data.user.pro = true;
-    anye.data.user.isVideoAvatarValid = true;
-    anye.data.user.ssvip = true;
-    anye.data.user.inProbationPeriod = true;
-    anye.data.user.svip = true;
-    anye.data.user.annualSsvip = true;
-    anye.data.user.annualVip = true;
-    };
+
 $done({body : JSON.stringify(anye)});
