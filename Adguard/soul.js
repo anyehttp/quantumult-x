a28a2787dacc2ff7e7cca907348f0a02d3309382
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-16 20:16:54 +0800

    更新 soul.js

--- Adguard/soul.js
+++ Adguard/soul.js
@@ -9,21 +9,22 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
+#^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
 ^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/soul.js
 [mitm]
 hostname = api-a.soulapp.cn
 *************************************/
 var anye = JSON.parse($response.body);
-const vip1 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo/;
-const vip2 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf/;
-
+//const vip1 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/getCardInfo/;
+//const vip2 = /^https:\/\/api-a\.soulapp\.cn\/loveBell\/queryMatchSpeedupConf/;
+/*
 if(vip1.test($request.url)){
     //anye.data.limitType = 1;
     anye.data.speedUp = true;
 }
-
 if(vip2.test($request.url)){
+*/
+
     //anye.data.limitType = 1;
     //速度?
     anye.data.currentSpeed = 100;
@@ -33,6 +34,5 @@ if(vip2.test($request.url)){
     anye.data.speedupCount = 9999;
     //加速否
     anye.data.speedup = true;
-}
 
 $done({ body: JSON.stringify(anye) });
