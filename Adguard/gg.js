758eb0c2c35660d889ee348edec8fc2127aa1c4e
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-19 09:38:48 +0800

    更新 gg.js

--- Adguard/gg.js
+++ Adguard/gg.js
@@ -13,12 +13,19 @@
 [rewrite_local]
 ^https:\/\/yt-user-api\.beijingcj\.cn\/\?_app url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/gg.js
 [mitm]
-hostname = yt-user-api.beijingcj.cn
+hostname = *.beijingcj.cn
 *************************************/
 var anye = JSON.parse($response.body);
-    anye.dataInfo.vipType = 1;
-    anye.dataInfo.nickName = "暗11夜";
-    anye.dataInfo.vipExpireTime = 4072440476;
+    anye.dataInfo.vipType = 7;
     anye.dataInfo.vipState = true;
     anye.dataInfo.newUser = true;
+	anye.dataInfo.vipExpireTime = 4072440476;
+	anye.dataInfo.setting.redNameSwitch = true;
+	console.log("viptype = " + anye.dataInfo.vipType);
+	console.log("nickName = " + anye.dataInfo.nickName);
+    console.log("vipState = " + anye.dataInfo.vipState);
+	console.log("newUser = " + anye.dataInfo.newUser);
+    console.log("vipExpireTime = " + anye.dataInfo.vipExpireTime);
+    console.log("redNameSwitch = " + anye.dataInfo.setting.redNameSwitch);
+
 $done({body: JSON.stringify(anye)});
