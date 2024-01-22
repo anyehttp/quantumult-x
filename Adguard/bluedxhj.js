1883e0f2b2d8be85aced261c4cb6c9cb5935be37
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-22 22:44:09 +0800

    更新 bluedxhj.js

--- Adguard/bluedxhj.js
+++ Adguard/bluedxhj.js
@@ -2,24 +2,12 @@
 #!name=blued
 #!desc=群组：https://t.me/IPAs_Dd 频道：https://t.me/IPAs_share 作者@anyeyey
 
-[URL Rewrite]
-https://social.blued.cn/users/recommend - reject-dict
-^https:\/\/social\.blued\.cn\/users\/no_auth\/benefit - reject-dict
-
 [Script]
 blued = type=http-response, pattern=^https:\/\/social\.blued\.cn\/users\/.*\/setting, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
 
-blued = type=http-response, pattern=^https:\/\/social\.blued\.cn\/users\/.*\/flash, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
-
-blued = type=http-response, pattern=^https:\/\/social\.blued\.cn\/users\?birth_time, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
-
-blued = type=http-response, pattern=^https:\/\/social\.blued\.cn\/users\/call\/state\?detail, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
-
-blued = type=http-response, pattern=^https:\/\/argo\.blued\.cn\/users\/, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
-
 blued = type=http-response, pattern=^https:\/\/social\.blued\.cn\/users\/shadow, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
 
-blued = type=http-response, pattern=^https:\/\/social\.blued\.cn\/users\/.+\/more\/ios\?v, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
+blued = type=http-response, pattern=^https:\/\/social\.blued\.cn\/users\/.*\/basi, script-path=https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js, requires-body=true, max-size=-1, timeout=60
 
 [MITM]
-hostname = %APPEND% social.blued.cn, argo.blued.cn
+hostname = %APPEND% social.blued.cn
