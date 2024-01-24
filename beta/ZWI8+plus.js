37107110bd88d1e7d0077d6554bf38678ac02651
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-25 07:36:48 +0800

    更新 ZWI8+plus.js

--- beta/ZWI8+plus.js
+++ beta/ZWI8+plus.js
@@ -70,7 +70,26 @@ class UserInfo {
                     "Authorization":this.token,
                     "serialId":''
                 },
-                body: `{"ziwiReward": {"type": "z_credit","amount": "1"}}`
+                body: `{
+  "jsonrpc": "2.0",
+  "id": 1706073615337,
+  "result": {
+    "record": {
+      "updateTime": null,
+      "id": 1842058,
+      "createTime": "2024-01-24 13:20:15",
+      "checkinTimes": 1,
+      "awards": [
+      ],
+      "checkinDate": "2024-01-24 13:20:15"
+    },
+    "hasAward": false,
+    "ziwiReward": {
+      "type": "z_credit",
+      "amount": "1"
+    }
+  }
+}`
             };
             //post方法
             let result = await httpRequest(options);
