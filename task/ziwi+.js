22dab76d449a2b8a3a4e9ddd62a4e9e99d2507a2
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-27 19:39:57 +0800

    Update ziwi+.js

--- task/ziwi+.js
+++ task/ziwi+.js
@@ -77,7 +77,6 @@ async function main() {
             //分享
             for (let i = 0; i < 10; i++) {
                 taskall.push(await user.fx());
-                await $.wait(user.getRandomTime());
             }
             
             await $.wait(user.getRandomTime());
