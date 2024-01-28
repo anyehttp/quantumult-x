484c80eb12ce597bfe4760594af88a41c6567a0d
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-01-28 13:37:41 +0800

    更新 ziwi+.js

--- task/ziwi+.js
+++ task/ziwi+.js
@@ -17,7 +17,7 @@
 
 手动添加定时任务
 [task_local]
-0 0,5,12 * * * https://raw.githubusercontent.com/anyehttp/quantumult-x/main/task/ziwi+.js, tag=微信小程序ziwi签到, enabled=true
+0 0,6,12 * * * https://raw.githubusercontent.com/anyehttp/quantumult-x/main/task/ziwi+.js, tag=微信小程序ziwi签到, enabled=true
 
 重写获取Cookie
 https://raw.githubusercontent.com/anyehttp/quantumult-x/main/task/ziwi-qx.conf
