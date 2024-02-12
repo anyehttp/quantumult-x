1fe86ab41fcc78404d485189dc439d7d5b2265f4
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-02-12 15:37:33 +0800

    更新 blued.js

--- Adguard/blued.js
+++ Adguard/blued.js
@@ -17,11 +17,11 @@ blued功能脚本  悄悄查看消息  地图无需展示头像即可查看全�
 **************************************
 
 [rewrite_local]
-^https:\/\/social\.blued\.cn\/users\/.*\/setting url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js
-^https:\/\/social\.blued\.cn\/users\/shadow url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js
-^https:\/\/social\.blued\.cn\/users\/.*\/basi url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js
+^https:\/\/*\.blued\.*\/users\/.*\/setting url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js
+^https:\/\/*\.blued\.*\/users\/shadow url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js
+^https:\/\/*\.blued\.*\/users\/.*\/basi url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/blued.js
 [mitm]
-hostname = social.blued.cn
+hostname = *.blued.*
 *************************************/
 var anye = JSON.parse($response.body);
 const vip1 = /^https:\/\/social\.blued\.cn\/users\/.*\/setting/;
