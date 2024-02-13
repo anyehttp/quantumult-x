97a8911826ada6e4050eaa23da77c6c700639ad2
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-02-13 20:50:00 +0800

    更新 aacsh.js

--- Adguard/aacsh.js
+++ Adguard/aacsh.js
@@ -8,21 +8,21 @@
 
 [rewrite_local]
 ^https:\/\/lifeserver\.clifes\.cn\/exam\/vip\/info url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/aacsh.js
-^https:\/\/lifeserver\.clifes\.cn\/user\/getUserInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/aacsh.js
 [mitm]
 hostname = lifeserver.clifes.cn
 *************************************/
 var anye = JSON.parse($response.body);
-const vip1 = /^https:\/\/lifeserver\.clifes\.cn\/exam\/vip\/info/;
-const vip2 = /^https:\/\/social\.blued\.cn\/users\/shadow/;
-if (vip1.test($request.url) && anye.data && anye.data.length > 0) {
+//^https:\/\/lifeserver\.clifes\.cn\/user\/getUserInfo url script-response-body https://raw.githubusercontent.com/anyehttp/quantumult-x/main/Adguard/aacsh.js
+//const vip1 = /^https:\/\/lifeserver\.clifes\.cn\/exam\/vip\/info/;
+//const vip2 = /^https:\/\/social\.blued\.cn\/users\/shadow/;
+//if (vip1.test($request.url) && anye.data && anye.data.length > 0) {
     anye.data = {
     "flag": true,
     "vVip": true,
     "buy": true
   }
-}
-if (vip2.test($request.url) && anye.data && anye.data.length > 0) {
-    anye.data.vVip = 1;
-}
+//}
+//if (vip2.test($request.url) && anye.data && anye.data.length > 0) {
+    //anye.data.vVip = 1;
+//}
 $done({ body: JSON.stringify(anye) });
