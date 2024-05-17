cfac2acbc9cf59b8ddecd7aead53bb11092d8eaf
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-05-18 01:47:32 +0800

    更新 sing-box.js

--- conf/sing-box/sing-box.js
+++ conf/sing-box/sing-box.js
@@ -1,7 +1,7 @@
 const { type, name } = $arguments
 const compatible_outbound = {
   tag: 'COMPATIBLE',
-  type: 'direct',
+  type: 'cn',
 }
 let compatible
 let config = JSON.parse($files[0])
@@ -15,6 +15,12 @@ config.outbounds.push(...proxies)
 config.outbounds.map(i => {
   if (['国内', 'gn-auto'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /China|china|中国/i))
+  }
+  if (['国内', 'gn'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /China|china|中国/i))
+  }
+  if (['国外', 'gw'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /JP|日本|美国|jp/i))
   }
   if (['国外', 'gw-auto'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /JP|日本|美国|jp/i))
