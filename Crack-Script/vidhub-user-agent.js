dbd562a580a8b2659b3707d32bde0e9fe4bee098
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-07-16 20:04:27 +0800

    更新 vidhub-user-agent.js

--- Crack-Script/vidhub-user-agent.js
+++ Crack-Script/vidhub-user-agent.js
@@ -1,3 +1,4 @@
 let anye = $request.headers;
-anye['User-Agent'] = 'infuse';
+anye['User-Agent'] = 'infuse/2024071204 CFNetwork/1329 Darwin/21.3.0';
+anye['user-agent'] = 'infuse/2024071204 CFNetwork/1329 Darwin/21.3.0'
 $done({headers: anye});
