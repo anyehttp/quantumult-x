97872e97539d457ef35833f9a617f56d14c2aaa5
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-08-15 18:10:51 +0800

    Create Steampy.js

---
+++ task/Steampy.js
@@ -0,0 +1,110 @@
+const $ = new Env('Steampy Price Checker');
+
+// 从环境变量获取数据
+const cookie = $.getdata('Steampy_Cookie');
+const accessToken = $.getdata('Steampy_accessToken');
+const steampyIds = $.getdata('Steampy_id').split('&');
+const steampyNames = $.getdata('Steampy_name').split('&');
+const notice = $.getdata('Steampy_notice') === 'true';
+
+// 模拟浏览器请求的请求头
+const headers = {
+    'Host': 'steampy.com',
+    'Content-Type': 'application/json;charset=UTF-8',
+    'Cookie': cookie,
+    'APP_TOKEN': 'App',
+    'User-Agent': 'BuniOS/1.2.19 (com.com.bun.www; build:0; iOS 15.3.1) Alamofire/4.9.1',
+    'accessToken': accessToken
+};
+
+// 异步请求 URL 获取 JSON 内容
+async function requestUrl(url) {
+    console.log(`请求 URL: ${url}`);
+    return new Promise((resolve, reject) => {
+        $.get({ url, headers }, (err, resp, body) => {
+            if (err) {
+                console.log(`请求错误: ${err}`);
+                reject(err);
+            } else {
+                console.log(`请求成功: ${url}`);
+                resolve(JSON.parse(body));
+            }
+        });
+    });
+}
+
+// 读取本地数据
+function readLocalData() {
+    const data = $.getdata('steampy_data') || '{}';
+    console.log('读取本地数据');
+    return JSON.parse(data);
+}
+
+// 写入本地数据
+function writeLocalData(data) {
+    $.setdata(JSON.stringify(data, null, 4), 'steampy_data');
+    console.log('写入本地数据');
+}
+
+// 获取游戏数据并处理价格
+async function getList(id, name) {
+    const url = `https://steampy.com/xboot/steamKeySale/listSale?pageNumber=1&pageSize=20&sort=keyPrice&order=asc&startDate=&endDate=&gameId=${id}`;
+    const resp = await requestUrl(url);
+
+    if (!resp.result || !resp.result.content || resp.result.content.length === 0) {
+        return `${name} 查询不到 cdkey 信息`;
+    }
+
+    const { steamName, ccy, keyPrice, stock } = resp.result.content[0];
+
+    let steampyData = readLocalData();
+    let steampyId = steampyData.Steampy_id || {};
+
+    if (!steampyId[id]) {
+        // ID 不存在，添加到数据中
+        steampyId[id] = {
+            appname: name,
+            appid: id,
+            price: keyPrice
+        };
+        steampyData.Steampy_id = steampyId;
+        writeLocalData(steampyData);
+        return `${name}-id:${id}-${steamName}-${ccy}-${keyPrice}-库存 ${stock}`;
+    } else {
+        // ID 存在，检查价格是否改变
+        const previousPrice = steampyId[id].price;
+        if (previousPrice !== keyPrice) {
+            // 价格发生变化，更新价格
+            steampyId[id].price = keyPrice;
+            writeLocalData(steampyData);
+            return `价格变化 ${name}-id:${id}-${steamName}-${ccy}-从 ${previousPrice} 变为 ${keyPrice}-库存 ${stock}`;
+        } else if (notice) {
+            // 价格无变化且通知开启
+            return `${name}-id:${id} 无变化`;
+        }
+    }
+
+    return '';
+}
+
+// 主函数
+async function main() {
+    const tasks = steampyIds.map((id, index) => getList(id, steampyNames[index]));
+    const results = await Promise.all(tasks);
+
+    const noticeMsg = results.filter(msg => msg).join('\n');
+    if (noticeMsg) {
+        $.msg('Steampy Price Checker', '', noticeMsg);
+        console.log(noticeMsg);
+    } else {
+        console.log('所有价格无变化');
+    }
+}
+
+main().catch(err => {
+    $.msg('Steampy Price Checker', '', `脚本执行出错: ${err.message}`);
+    console.log(`脚本执行出错: ${err.message}`);
+});
+
+// 必须的类定义
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`🔔${this.name}, 开始!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}isShadowrocket(){return"undefined"!=typeof $rocket}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){if(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:i,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:i,statusCode:r,headers:o,rawBody:h},s.decode(h,this.encoding))},t=>{const{message:i,response:r}=t;e(i,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let i=require("iconv-lite");this.initGotEnv(t);const{url:r,...o}=t;this.got[s](r,o)。then(t=>{const{statusCode:s，statusCode:r，headers:o,rawBody:h}=t;e(null，{status:s,statusCode:r,headers:o,rawBody:h},i.decode(h,this。encoding))}，t=>{const{message:s,response:r}=t;e(s，r，r&&i.decode(r.rawBody，this。encoding))})}}time(t，e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1，"d+":s.getDate()，"H+":s.getHours()，"m+":s.getMinutes()，"s+":s.getSeconds()，"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/。test(t)&&(t=t.replace(RegExp.$1，(s.getFullYear()+"")。substr(4-RegExp.$1。length)));for(let e in i)new RegExp("("+e+")")。test(t)&&(t=t.replace(RegExp.$1，1==RegExp.$1。length?i[e]:("00"+i[e])。substr((""+i[e])。length)));return t}msg(e=t,s=""，i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this。isQuanX()?{"open-url":t}:this。isSurge()?{url:t}:void 0;if("object"==typeof t){if(this。isLoon()){let e=t.openUrl||t.url||t["open-url"]，s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this。isQuanX()){let e=t["open-url"]||t.url||t.openUrl，s=t["media-url"]||t.mediaUrl，i=rawOpts["update-pasteboard"]||rawOpts.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":i}}if(this。isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this。isMute||(this。isSurge()||this。isLoon()?$notification.post(e,s,i,o(r)):this。isQuanX()&&$notify(e,s,i,o(r))),!this。isMuteLog){let t=[""，"==============📣系统通知📣=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n"))，this.logs=this。logs。concat(t)}}log(...t){t.length>0&&(this。logs=[...this。logs,...t]),console.log(t.join(this。logSeparator))}logErr(t，e){const s=!this。isSurge()&&!this。isQuanX()&&!this。isLoon();s?this。log(""，`❗️${this。name}, 错误!`,t.stack):this.log(""，`❗️${this。name}, 错误!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date)。getTime()，s=(e-this。startTime)/1e3;this。log(""，`🔔${this。name}, 结束! 🕛 ${s} 秒`)，this。log()，(this。isSurge()||this。isQuanX()||this。isLoon())&&$done(t)}}(t，e)}
