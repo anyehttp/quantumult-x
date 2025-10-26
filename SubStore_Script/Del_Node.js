e60e816ec238152c0ce055ef047670d6f393bd4b
anyehttp <93598563+anyehttp@users.noreply.github.com>
2025-10-26 16:54:46 +0800

    更新 Del_Node.js

--- SubStore_Script/Del_Node.js
+++ SubStore_Script/Del_Node.js
@@ -1,7 +1,11 @@
 function operator(proxies = [], targetPlatform, context) {
-  // 过滤掉没有 obfs-opts 字段的节点
+  // 过滤条件：1. 存在 obfs-opts 字段 2. obfs-opts 下存在 mode 字段
   const filteredProxies = proxies.filter(proxy => {
-    return proxy['obfs-opts'] !== undefined && proxy['obfs-opts'] !== null;
+    // 先判断 obfs-opts 存在且不为 null，再判断其下的 mode 存在且不为 null/undefined
+    return proxy['obfs-opts'] !== undefined && 
+           proxy['obfs-opts'] !== null && 
+           proxy['obfs-opts'].mode !== undefined && 
+           proxy['obfs-opts'].mode !== null;
   });
   
   console.log(`原始节点数: ${proxies.length}, 过滤后节点数: ${filteredProxies.length}`);
